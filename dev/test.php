687d380fea8f785f0ee7d3c2c13ca55ff5a6aeef
AdamVig <adamvigneaux@gmail.com>
2014-03-13 16:13:48 -0400

    Testing Heroku

--- dev/test.php
+++ dev/test.php
@@ -2,10 +2,9 @@
 
 require_once "../make-schedule.php";
 require_once "../index.php";
-/*
 require_once "twilio-php/Services/Twilio.php";
 $client = new Services_Twilio("AC4c45ba306f764d2327fe824ec0e46347", "5121fd9da17339d86bf624f9fabefebe");
-*/
+
 $u = array(
     'ID'=> '1',
     'Number'=> '1',
@@ -26,5 +25,5 @@ $u = array(
 
 //$d = getDaySchedule('monday', openCalendar(), "tahanto");
 $d = "D Er A,D,E,F";
-echo makeTahanto($u, $d);
 
+report($client, "Testing from Heroku!");
