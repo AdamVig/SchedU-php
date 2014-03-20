fdd8f6d46deccc97c15a1969bec910dfc860c583
AdamVig <adamvigneaux@gmail.com>
2014-03-20 15:03:32 -0400

    Add composer

--- dev/test.php
+++ dev/test.php
@@ -28,4 +28,4 @@ $u = array(
 //$d = getDaySchedule('monday', openCalendar(), "tahanto");
 $d = "D Er A,D,E,F";
 
-report($client, "Testing from Heroku!");
+report($client, "Testing from Heroku on March 20!");
