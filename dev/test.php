a1d78d565016f66cdffd3f7857a38e1a1d0820f7
AdamVig <adamvigneaux@gmail.com>
2014-03-21 18:23:49 -0400

    add /app to paths

--- dev/test.php
+++ dev/test.php
@@ -2,9 +2,9 @@
 error_reporting(-1);
 ini_set('display_errors', 1);
 ini_set('html_errors', 1);
-require_once "/www/make-schedule.php";
-require_once "/www/index.php";
-require_once "/www/vendor/autoload.php";
+require_once "/app/www/make-schedule.php";
+require_once "/app/www/index.php";
+require_once "/app/www/vendor/autoload.php";
 $client = new Services_Twilio("AC4c45ba306f764d2327fe824ec0e46347", "5121fd9da17339d86bf624f9fabefebe");
 
 $u = array(
@@ -25,7 +25,6 @@ $u = array(
     'G'=> 'Calculus'
 );
 
-//$d = getDaySchedule('monday', openCalendar(), "tahanto");
 $d = "D Er A,D,E,F";
 
-report($client, "Testing from Heroku on March 20, minor change here!");
+report($client, "Testing from Heroku on March 21.");
