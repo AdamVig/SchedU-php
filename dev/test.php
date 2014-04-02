442e71977235d13281c756b0c8af248b03aa3ec3
AdamVig <adamvigneaux@gmail.com>
2014-04-02 08:48:40 -0400

    Add database test, remove Guzzle test

--- dev/test.php
+++ dev/test.php
@@ -7,56 +7,23 @@ require_once "../vendor/autoload.php";
 // require_once "../index.php";
 //$client = new Services_Twilio("AC4c45ba306f764d2327fe824ec0e46347", "5121fd9da17339d86bf624f9fabefebe");
 
-$accountId = 'AC4c45ba306f764d2327fe824ec0e46347';
-$accountKey = '5121fd9da17339d86bf624f9fabefebe';
-$url = "https://$accountId:$accountKey@api.twilio.com/2010-04-01/Accounts/$accountId/Messages";
-$client = new GuzzleHttp\Client();
-
-$data = [
-  'body' => [
-    'From' => '+16505427238',
-    'To' => '+15086884042',
-    'Body' => 'Testing!'
-  ]
+$db = [
+    'host' => 'localhost',
+    'username' => 'schedu',
+    'password' => 'schedu'
 ];
+$database = new mysqli($db['host'], $db['username'], $db['password']);
+$query = "SELECT * FROM users";
+$result = $database->query($query);
+//------------------------------------------------------
 
-//$client->post($url, $data)->send();
 
-$requests = [
-    $client->createRequest('POST', $url, [
-        'body' => [
-            'From' => '+16505427238',
-            'To' => '+15086884042',
-            'Body' => 'Testing!'
-        ]
-    ]),
-    $client->createRequest('POST', $url, [
-        'body' => [
-            'From' => '+16505427238',
-            'To' => '+15083080173',
-            'Body' => 'Testing!'
-        ]
-    ]),
-    $client->createRequest('POST', $url, [
-        'body' => [
-            'From' => '+16505427238',
-            'To' => '+19788702867',
-            'Body' => 'Testing!'
-        ]
-    ])
-];
-
-$client->sendAll($requests, [
-    'error' => function (ErrorEvent $event) use (&$errors) {
-        $errors[] = $event;
-    }
-]);
-
-$message = "SchedU Errors Today:<br>";
-foreach ($errors as $error) {
-    $message .= $error."<br>";
+//------------------------------------------------------
+//GO THROUGH DATABASE ARRAY
+while ($userData = $result->fetch_assoc()) {
+    print_r($userData);
+    echo "<br><br>";
 }
-mail("adam@getschedu.com", "SchedU Errors Today", $message);
 
 $u = [
   'ID'=> '1',
